684b4a38be32e75f507de9d093758d48c2c96e8f
Farrasnaufalsuhanda <farrasnaufalsuhanda14@gmail.com>
2023-06-25 18:58:20 +0700

    new

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const sequelize = require ("sequelize");
 // create connection
 const db = new sequelize('signing', 'root', '', {
     host: 'localhost',
-    port: '3307',
+    port: '3306',
     dialect: 'mysql',
     dialectOptions: {
         options: {
